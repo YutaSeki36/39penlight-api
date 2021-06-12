3140b703b7d8092bdc0b62e85fef1cb762ce2a80
YutaSeki36 <sekiyuta0306@gmail.com>
2021-06-12 18:05:16 +0900

    cors対応

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.14
 
 require (
 	github.com/eclipse/paho.mqtt.golang v1.3.4
+	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.7.2
 	golang.org/x/net v0.0.0-20210525063256-abc453219eb5 // indirect
 )
--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"github.com/39penlight-api/mqtt"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"os"
+	"time"
 )
 
 func main() {
@@ -12,6 +14,25 @@ func main() {
 	controller := NewColorController(cl)
 
 	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{
+			"https://zen-fermat-8df333.netlify.app",
+			"http://localhost:3000",
+		},
+		AllowMethods: []string{
+			"POST",
+		},
+		AllowHeaders: []string{
+			"Access-Control-Allow-Credentials",
+			"Access-Control-Allow-Headers",
+			"Content-Type",
+			"Content-Length",
+			"Accept-Encoding",
+			"Authorization",
+		},
+		AllowCredentials: true,
+		MaxAge:           24 * time.Hour,
+	}))
 
 	r.POST("color", func(context *gin.Context) {
 		controller.ChangePenlightColor(context)
