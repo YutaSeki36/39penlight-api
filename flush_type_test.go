0add32cdf35cdc0856cb2610913ea073fcae9bb1
YutaSeki36 <sekiyuta0306@gmail.com>
2021-06-18 23:55:35 +0900

    flush_type異常系テスト追加

--- flush_type_test.go
+++ flush_type_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestNewFlushTypeSuccess(t *testing.T) {
 	testCase := []int{
@@ -11,6 +14,24 @@ func TestNewFlushTypeSuccess(t *testing.T) {
 		_, err := NewFlushType(v)
 		if err != nil {
 			t.Fatalf("failed test %#v", err)
+		}
+	}
+}
+
+func TestNewFlushTypeFailed(t *testing.T) {
+	testCase := []int{
+		0,
+		-1,
+		3,
+		100,
+	}
+	for _, v := range testCase {
+		_, err := NewFlushType(v)
+		if err == nil {
+			t.Fatalf("failed test %#v", "エラーが発生しませんでした．")
+		}
+		if err.Error() != fmt.Sprintf("不明なフラッシュタイプ. 値: %d", v) {
+			t.Fatalf("failed test %#v", "想定するエラーではありません．")
 		}
 	}
 }
